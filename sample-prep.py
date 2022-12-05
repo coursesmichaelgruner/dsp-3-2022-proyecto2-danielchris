5d1245d7b3354b0789ec37446bb99b382200d49b
Daniel Rojas <159dram@gmail.com>
2022-12-05 13:08:31 -0600

    fixes order of scripts

--- sample-prep.py
+++ sample-prep.py
@@ -1,8 +1,8 @@
 import subprocess
 
 
-subprocess.call(["python","background_noise_slice.py"])
 subprocess.call(["python","validation_sort_script.py"])
 subprocess.call(["python","training_sort_script.py"])
+subprocess.call(["python","background_noise_slice.py"])
 subprocess.call(["python","fill_sounds.py"])
 subprocess.call(["python","build_spectrograms.py"])
